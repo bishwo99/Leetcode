64a5be37134a584c95b7e1a1648e8dd5df969126
bishwo99 <112264657+bishwo99@users.noreply.github.com>
2025-01-11 22:42:23 +0600

    Add files via upload

---
+++ 219_Contains_Duplicate.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++) cin >> nums[i];
+    int k; 
+    cin >> k;
+    bool ans = false;
+    
+    for(int i = 0; i < n; i++)
+    {
+        for( int j = i+1; j < n; j++ )
+        {
+            if( nums[i] == nums[j]){
+                if(abs(j - i) <= k){
+                      ans = true;
+                };
+            }
+        }
+    }
+
+    if(ans) cout <<"True";
+    else 
+    cout <<"False";
+
+
+
+
+
+}
+
+
+
