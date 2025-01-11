cfbebe149ed6b9a464c93ef95c28949b730d4e0f
bishwo99 <112264657+bishwo99@users.noreply.github.com>
2025-01-11 21:17:42 +0600

    Add files via upload

---
+++ 2357_Make_array_zero_by_Subrtracting_Equal_element.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int n;
+   cin >> n;
+   vector<int> nums(n);
+   for(int i = 0; i < n; i ++) cin >> nums[i];
+   
+   unordered_set<int> st;
+   for( auto it : nums ){
+    if(it != 0) {
+        st.insert(it);
+    }
+   }
+   
+ 
+}
